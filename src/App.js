5362138fb9b4925481498b2011593052533ed8c2
Anthony Astige <anthonyastige@gmail.com>
2017-09-01 12:59:47 -0500

    Basic green / red split

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Lamp from './components/Lamp.js'
 
 class App extends Component {
 	render() {
-		return <Lamp color="red" lit="true" />
+		return <Lamp color="red" lit={false} />
 	}
 }
 
--- src/components/Lamp.js
+++ src/components/Lamp.js
@@ -1,14 +1,42 @@
 import React from 'react'
 
+const lightBaseStyle = {
+	width: '100px',
+	height: '100px',
+	margin: '10px',
+	borderRadius: '50px'
+}
+const RedLight = props => {
+	const style = Object.assign({}, lightBaseStyle, {
+		backgroundColor: props.color,
+		opacity: props.lit ? 1 : 0.1
+	})
+	return <div style={style} />
+}
+
+const GreenLight = props => {
+	const style = Object.assign({}, lightBaseStyle, {
+		backgroundColor: props.color,
+		opacity: props.lit ? 1 : 0.1
+	})
+	return <div style={style} />
+}
+
 const Lamp = props => {
 	const style = {
-		width: '100px',
-		height: '100px',
-		backgroundColor: props.color,
-		opacity: props.lit ? 1 : 0.1,
-		borderRadius: '50px'
+		width: '120px',
+		height: '240px',
+		borderRadius: '5px',
+		border: '1px solid #666'
 	}
-	return <div style={style} />
+	const greenColor = '#27e833'
+
+	return (
+		<div style={style}>
+			<GreenLight lit={!props.lit} color={greenColor} />
+			<RedLight lit={props.lit} color={props.color} />
+		</div>
+	)
 }
 
 export default Lamp
