a5c349a3df6997cd4ecc30111f1a8d76339e60eb
Anthony Astige <anthonyastige@gmail.com>
2017-09-01 13:10:17 -0500

    Toggle lit state like in example to help me code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react'
 import Lamp from './components/Lamp.js'
 
 class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			lit: false
+		}
+		this.toggleLit = this.toggleLit.bind(this)
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(this.toggleLit, 1000)
+	}
+
+	toggleLit() {
+		this.setState({ lit: !this.state.lit })
+	}
+
 	render() {
-		return <Lamp color="red" lit={false} />
+		return <Lamp color="red" lit={this.state.lit} />
 	}
 }
 
